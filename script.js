3f4e8fed6e03c952b00081a2e8013c5db8a53397
JulianK123 <julian.kruk@stud.vilniustech.lt>
2023-12-08 15:28:00 +0200

    padarytas kitas variantas

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-
-function checkMarks(marks, marksArr){
-    for(let i = 0; i < marks.length;i++){
-        if(marks[i]<=4){
-            marksArr[i].style.color = "red";
-        }else if(marks[i]>4 && marks[i] <= 8){
-            marksArr[i].style.color = "orange";
-        }else{
-            marksArr[i].style.color = "green";
+function romanize (num) {
+    if (isNaN(num))
+        return NaN;
+    var digits = String(+num).split(""),
+        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+               "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+    return Array(+digits.join("") + 1).join("M") + roman;
+}
+function check(nums) {
+    for(let i = 0; i< nums.length;i++){
+        // console.log(/^-?\d+$/.test(nums[i]));
+        if(!(/^\d+$/.test(nums[i]))){
+            // console.log("ab0");
+             return false;
         }
-        marksArr[i].innerHTML = marks[i]+", ";
+        
     }
+    return true;
+}
+
+function color(num, sum){
+
+    // for(let i = 0; i < nums.length;i++){
+    //     if(nums[i]<=7){
+    //         sum.style.color = "red";}
+    //     // }else if(marks[i]>4 && marks[i] <= 8){
+    //     //     marksArr[i].style.color = "orange";
+    //     // }else{
+    //     //     marksArr[i].style.color = "green";
+    //     // }
+    //     marksArr[i].innerHTML = marks[i]+", ";
+    // }
+    if(num <= 7) sum.style.color = "red";
+    else if(num <=14 && num>7) sum.style.color = "orange";
+    else sum.style.color = "green";
+
 }
 function checkCode(code){
     let isnum = code.match(/^[0-9]+$/);
@@ -30,7 +59,6 @@ window.onload = () => {
     const info = {
         name: document.getElementsByName("name"),
         surname: document.getElementsByName("surName"),
-        code: document.getElementsByName("stCode"),
         grades: document.getElementById("grades").getElementsByClassName("grade"),
         labels: document.getElementById("form").getElementsByClassName("label"),
     }
@@ -38,18 +66,25 @@ window.onload = () => {
     
     let button = document.getElementById("calculate-button");
     button.onclick = ()=>{    
-        if(!checkCode(Object.values(info)[2][0].value)){
-            for(let i = 0; i<3;i++){
-                console.log(info.labels[i].textContent);
+        let numbers = [];
+            for(let i = 0; i<info.grades.length;i++)
+                numbers[i] = info.grades[i].value;
+            // console.log(info[0])
+            if(!check(numbers)) alert("It is not a number");
+            else{
+            for(let i = 0; i<2;i++){
+                // console.log(info.labels[i].textContent);
                 console.log((Object.values(info)[i][0].value));
             }
             
-            console.log("Marks:");
+            console.log("Numbers:");
             let gradesStr = "";
             for(let i = 0; i<info.grades.length;i++)
-            gradesStr += info.grades[i].value+", ";
+                gradesStr += info.grades[i].value+", ";
             
             console.log(gradesStr);
+
+            
             
             let name = document.getElementById("name");
             name.innerHTML = info.labels[0].textContent + " " + (Object.values(info)[0][0].value);
@@ -57,8 +92,6 @@ window.onload = () => {
             let surname = document.getElementById("surName");
             surname.innerHTML = info.labels[1].textContent + " " + (Object.values(info)[1][0].value);
 
-            let code = document.getElementById("stCode");
-            code.innerHTML = info.labels[2].textContent + " " + (Object.values(info)[2][0].value);
 
             let marks = [];
             for(let i = 0; i<info.grades.length;i++){
@@ -70,26 +103,36 @@ window.onload = () => {
 
             
             let marksNode = document.getElementById("marks");
-            marksNode.innerHTML = "Marks:" + "<br />";
-            let marksArr = document.getElementsByClassName("mark");
-            
-            checkMarks(marks, marksArr);
+            let sum = document.getElementById("sum");
 
-            let bestMark = document.getElementById("bestMark");
-            bestMark.innerHTML = "<br />Best mark of " + Object.values(info)[0][0].value +" "+ Object.values(info)[1][0].value
-            +"("+ Object.values(info)[2][0].value +"): " + maxGr + "<br /> <br />";
+            let summ = 0;
+            for(let i = 0; i<info.grades.length;i++)
+                summ += parseInt(info.grades[i].value);
+            console.log(summ);
+            sum.innerHTML = romanize(summ);
+            marksNode.innerHTML = "Numbers:" + "<br />" + gradesStr;
+
+           color(summ,sum);
             
-            let worstMark = document.getElementById("worstMark");
-            worstMark.innerHTML = "worst: " + minGr + "<br />";
             
-            let colors = [];
-            for(let i = 0; i<info.grades.length;i++){
-                marks[i] = info.grades[i].value;
-            }
+            
+            // checkMarks(marks, marksArr);
+
+            // let bestMark = document.getElementById("bestMark");
+            // bestMark.innerHTML = "<br />Best mark of " + Object.values(info)[0][0].value +" "+ Object.values(info)[1][0].value
+            // +"("+ Object.values(info)[2][0].value +"): " + maxGr + "<br /> <br />";
+            
+            // let worstMark = document.getElementById("worstMark");
+            // worstMark.innerHTML = "worst: " + minGr + "<br />";
+            
+            // let colors = [];
+            // for(let i = 0; i<info.grades.length;i++){
+            //     marks[i] = info.grades[i].value;
+            // }
         
-        };
         
         
-    
+        
+        }
     }
 };
